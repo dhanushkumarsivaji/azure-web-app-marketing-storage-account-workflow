c8889b0f518156d21946a2e11de8110286cbd9a3
Dhanush S <dhas@WKMZT7418515.global.publicisgroupe.net>
2023-01-30 09:25:16 +0530

    Updated the prod webpack config file

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,11 +3,13 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common');
 
+const domain = 'https://azwebappgithubworkflow.z13.web.core.windows.net'
+
 const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: 'https://azwebappgithubworkflow.z13.web.core.windows.net/marketing/latest/',
+    publicPath: `${domain}/marketing/latest/`,
   },
   plugins: [
     new ModuleFederationPlugin({
